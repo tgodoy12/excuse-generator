b86d21494716e783f72b8677d942065dd50ca610
tgodoy12 <112527761+tgodoy12@users.noreply.github.com>
2024-06-04 22:00:28 +0000

    first commit

--- src/app.js
+++ src/app.js
@@ -2,10 +2,35 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let excuse = generateExcuse();
+  document.querySelector("#excuse").innerHTML = excuse;
 };
+
+function generateExcuse() {
+  let who = ["The dog", "My grandma", "The mailman", "My bird"];
+  let action = [" ate", " peed", " crushed", " broke"];
+  let what = [" my homework", " my phone", " the car"];
+  let when = [
+    " before the class",
+    " when I was sleeping",
+    " while I was exercising",
+    " during my lunch",
+    " while I was praying"
+  ];
+
+  let sentences = [who, action, what, when];
+
+  let randomNumber = "";
+  let excuse = "";
+
+  for (let i = 0; i < sentences.length; i++) {
+    for (let j = 0; j < 1; j++) {
+      randomNumber = Math.floor(Math.random() * sentences[i].length);
+      excuse += sentences[i][randomNumber];
+    }
+  }
+
+  return excuse;
+}
