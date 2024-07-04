9e736e2e1f727e36795a82c04475224f202f8d8b
tgodoy12 <112527761+tgodoy12@users.noreply.github.com>
2024-07-04 13:31:30 +0000

    comments on the app.js added

--- src/app.js
+++ src/app.js
@@ -9,6 +9,7 @@ window.onload = function() {
 };
 
 function generateExcuse() {
+  //declaring arrays with the who, what, when and action
   let who = ["The dog", "My grandma", "The mailman", "My bird"];
   let action = [" ate", " peed", " crushed", " broke"];
   let what = [" my homework", " my phone", " the car"];
@@ -20,11 +21,17 @@ function generateExcuse() {
     " while I was praying"
   ];
 
+  //declaring an array with the arrays declared before
   let sentences = [who, action, what, when];
 
+  
   let randomNumber = "";
+
+  //the sentense will be concatenated in the excuse variable
   let excuse = "";
 
+  //first for will be for the sentences array 
+  //and second for will be for the content of each arrays
   for (let i = 0; i < sentences.length; i++) {
     for (let j = 0; j < 1; j++) {
       randomNumber = Math.floor(Math.random() * sentences[i].length);
